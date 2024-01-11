1d0d54813d45261b474e460bbab62550ba7c9d68
Christian Hey <c.hey@sbs-herzogenaurach.de>
2024-01-11 14:58:29 +0100

    Added task 6

--- Uebung.py
+++ Uebung.py
@@ -28,6 +28,15 @@ def addUserToList(listOfUserObjects):
 # Jahr 2022 ihr Kennwort nicht geändert haben, werden deaktiviert, in dem der Key valid
 # den Value False erhält.
 
+# Aufgabe 6.
+#
+# Erstellen Sie eine Funktion setValid mit dem Parameter listOfUserObjects. Der Parameter
+# beinhaltet eine Liste mit allen Benutzerobjekten.
+#
+# Gehen Sie alle Benutzer der Liste des Parameter durch. Jeder Benutzer, welcher
+# deaktivert wurde (siehe Aufgabe 5) erhält ein neues Passwort. Das Passwort gibt der
+# Anwender über einen input ein. Zudem wird der Value für den valid-Key auf True gesetzt.
+
 # In dem folgenden Bereich nehmen Sie keine Änderungen vor!
 # Testdaten:
 userList = list()
@@ -39,6 +48,8 @@ user["valid"] = True
 user["passwordChanged"] = datetime(2021, 2, 21)
 user["e-mail"] = "peter@web.de"
 
+userList.append(user)
+
 user = dict()
 user["username"] = "franz.huber"
 user["password"] = "Passwort"
@@ -46,12 +57,16 @@ user["valid"] = False
 user["passwordChanged"] = datetime(2023, 3, 1)
 user["e-mail"] = "hubi@gmx.de"
 
+userList.append(user)
+
 user = dict()
 user["username"] = "julia.meyer"
 user["password"] = "abc123"
 user["valid"] = True
 user["passwordChanged"] = datetime(2024, 1, 2)
 user["e-mail"] = "jule@meyer.de"
+
+userList.append(user)
 
 
 # Ab hier können Sie Ihre Lösungen testen:
